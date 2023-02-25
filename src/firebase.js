f114040d87123244203cc4d7d322d32748db7c46
Noibisjunior <noibisjunior22@gmail.com>
2023-02-25 12:34:14 +0100

    Thanks

--- src/firebase.js
+++ src/firebase.js
@@ -26,7 +26,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"
 
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
